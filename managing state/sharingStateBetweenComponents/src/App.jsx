8b6203d86e95a30e6af3cba50c8e48b4bdf2ec36
Prince Mahmood <princemahmood117@gmail.com>
2025-11-27 12:57:30 -0800

    src updated

--- managing state/sharingStateBetweenComponents/src/App.jsx
+++ managing state/sharingStateBetweenComponents/src/App.jsx
@@ -1,9 +1,31 @@
 import Accordian from "./components/Accordian";
+<<<<<<< HEAD
+import Counter from "./components/Counter";
+import Different from "./components/Different";
+import Fancy from "./components/Fancy";
+import SecondCounter from "./components/SecondCounter";
+=======
+>>>>>>> dc7a9e29b7b70685dbacc179f98da412c790c3c3
 
 
 function App() {
   return <>
+<<<<<<< HEAD
+  {/* <Accordian></Accordian> */}
+
+  <div>
+    {/* <Counter></Counter>
+    <Counter></Counter> */}
+  </div>
+
+  {/* <SecondCounter></SecondCounter> */}
+  
+  {/* <Fancy></Fancy> */}
+
+  <Different></Different>
+=======
   <Accordian></Accordian>
+>>>>>>> dc7a9e29b7b70685dbacc179f98da412c790c3c3
   
   
   </>;
