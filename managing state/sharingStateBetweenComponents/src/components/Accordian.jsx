dc7a9e29b7b70685dbacc179f98da412c790c3c3
iftekhar <princemahmood117@gmail.com>
2025-11-27 18:10:43 +0600

    s

--- managing state/sharingStateBetweenComponents/src/components/Accordian.jsx
+++ managing state/sharingStateBetweenComponents/src/components/Accordian.jsx
@@ -14,8 +14,11 @@ const [activeIndex, setActiveIndex] = useState(0);
       </Pannel>
 
       <Pannel title={'Free Course'} isActive={activeIndex === 1 && true} handleActive={() => setActiveIndex(1)}>
-        Leo cras beatae exercitationem quibusdam cumque. Tempore massa minus, pariatur. Sapien esse ultrices aut bibendum magnis cillum lacinia, itaque repudiandae possimus. Ac mollitia purus, nascetur, sociis pharetra montes labore, torquent!
-      
+        Leo cras beatae exercitationem quibusdam cumque. Tempore massa minus, pariatur. Sapien esse ultrices aut bibendum magnis cillum lacinia, itaque repudiandae possimus. Ac mollitia purus, nascetur, sociis pharetra montes labore, torquent!      
+      </Pannel>
+
+      <Pannel title={'Free Course'} isActive={activeIndex === 2 && true} handleActive={() => setActiveIndex(2)}>
+        Leo cras beatae exercitationem quibusdam cumque. Tempore massa minus, pariatur. Sapien esse ultrices aut bibendum magnis cillum lacinia, itaque repudiandae possimus. Ac mollitia purus, nascetur, sociis pharetra montes labore, torquent!      
       </Pannel>
     </>
   );
